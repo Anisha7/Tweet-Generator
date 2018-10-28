0750b337653f85b2e9184e785f5302e8317ff203
Anisha Jain <anishajain@outlook.com>
2018-10-27 20:11:00 -0700

    anagrams base

---
+++ anagrams.py
@@ -0,0 +1,19 @@
+
+# take a word
+# rearrange it and check if in dictionary
+# return new word
+
+def makeAnagram(word, d):
+    return
+
+def run:
+    words = int(sys.argv[1])
+    file = open('/usr/share/dict/words','r')
+    content = list(file)
+
+    gameState = input('Do you want to play? (y/n): ')
+    while (gameState == 'Y' or gameState == 'y'):
+        #do stuff
+        return
+
+    return
