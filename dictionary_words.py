c7380c0968afee4651512e7659c0e4afe9673f81
Anisha Jain <anishajain@outlook.com>
2018-10-27 19:06:32 -0700

    bug fixes:works now

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,17 +1,17 @@
 # use words from file: /usr/share/dict/words
 # Take 1 argument: amount of words to select
 # put the words together in a 'sentence' (order doesn't matter, it doesn't have to make sense)
-import sys;
-import random;
+import sys
+import random
 
-words = int(sys.argv[1]);
-file = open('/usr/share/dict/words','r');
-content = list(file);
+words = int(sys.argv[1])
+file = open('/usr/share/dict/words','r')
+content = list(file)
 
-result = "";
+result = ""
 while (words > 0) :
-    index = random.randint(0, len(content) - 1);
-    result += content[index].strip() + " ";
-    words -= 1;
+    index = random.randint(0, len(content) - 1)
+    result += content[index].strip() + " "
+    words -= 1
 
-print(result);
+print(result)
