b16248abeac1445d585285bee7b9388153edba62
Anisha Jain <anishajain@outlook.com>
2018-11-01 14:49:55 -0700

    implemented a random function

--- stoch_sampling_hist.py
+++ stoch_sampling_hist.py
@@ -1,13 +1,53 @@
+
+
 import sys
 import random
 import os
+
+# counts total words (NOT UNIQUE)
+def countWords(hist):
+    # tot count
+    totCount = 0
+    for k in hist.keys():
+        totCount += hist[k]
+    return totCount
+
+# randomly returns a word taking probability in consideration
+def randomArg(hist):
+    # tot count
+    totCount = countWords(hist)
+    
+    
+    random_num = random.randint(0, totCount-1)
+
+    for k in hist.keys():
+        random_num -= hist[k]
+        if (random_num < 0):
+            return k
+
+    # should never get here
+    return hist.keys()[0]
+
+
+# returns a dictionary with key: word, value: number of occurances
+# note: check for punctuation 'of?'
+def histogram(content) :
+    words_dict = dict()
+    
+    for i in range(len(content)):
+        if inDict(content[i], words_dict):
+            words_dict[content[i]] = 1 + words_dict[content[i]]
+        else : 
+            words_dict[content[i]] = 1
+   
+    return words_dict
 
 # returns true if path is a file
 def isFile(path):
     return os.path.isfile(path)
 
 # read file if it is a file 
-# convert it to a list of words
+# convert it to a dictionary histogram
 def readFile(path, args):
     if (isFile(path)):
         # read file
@@ -15,13 +55,7 @@ def readFile(path, args):
         with open(path) as file:
             content = (file.read()).split()
         args = content
-    
-    return args
-
-# get random word from list of words (args)
-def randomArg(args):
-    n = random.randint(0, len(args)-1)
-    return args[n]
+    return histogram(args)
 
 # checks if word is in dictionary
 def inDict(word, d):
@@ -47,9 +81,6 @@ def probability(d, key, n):
     prob = d[key]/n
     return round(prob, 3)
 
-
-
-
 def printProbabilities(d, n):
     for k in d.keys():
         result = k + ": "
@@ -62,6 +93,22 @@ def printDict(d):
         result = k + ": " + str(d[k])
         print(result)
 
+def expectedProbabilities(hist):
+    totCount = countWords(hist)
+    expectedProb = dict()
+    for k in hist.keys():
+        prob = round(hist[k]/totCount, 2)
+        expectedProb[k] = prob
+    
+    return expectedProb
+
+def printExpected(expectedProb):
+
+    for k in expectedProb.keys():
+        result = k + ": " + str(expectedProb[k])
+        print(result)
+    return
+
 def run():
     args = sys.argv[1:]
     path = args[0]
@@ -70,6 +117,10 @@ def run():
     #print(randomArg(args))
     n = 100000
     d = sample(args, n)
+
+    print("Expected:")
+    printExpected(expectedProbabilities(args))
+
     print("Probabilities: ")
     printProbabilities(d, n)
     print("occurances for " + str(n) + " cases: ")
@@ -77,4 +128,5 @@ def run():
 
 
 run()
+
 
